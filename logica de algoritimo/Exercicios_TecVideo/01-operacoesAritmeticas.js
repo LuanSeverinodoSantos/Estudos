41bc110ed8563a313b63c3f6e6d6deef43ccde74
Lss. <68402328+LuanSeverinodoSantos@users.noreply.github.com>
2026-03-02 23:12:00 -0300

    Começando os exercicios do gustavo caetan de 2022 de js

---
+++ logica de algoritimo/Exercicios_TecVideo/01-operacoesAritmeticas.js
@@ -0,0 +1,8 @@
+// EXERCÍCIO 1
+// Faça as 4 operações básica com base nos números 2 e 4:
+// Ao final de cada operação imprima na tela no seguinte formato:
+// Soma: 2 + 4 = x
+// Subtração : 2 - 4 = x
+// Divisão: 2 / 4 = x
+// Multiplicação: 2 * 4 = x
+// ---------------------------------------------------------------
