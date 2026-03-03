b7b00cd50777b8aacef95fc8d6302826d40ba324
Lss. <68402328+LuanSeverinodoSantos@users.noreply.github.com>
2026-03-02 23:28:05 -0300

    Terminado o exercicio 01 tem mais alguns para terminar

--- logica de algoritimo/Exercicios_TecVideo/01-operacoesAritmeticas.js
+++ logica de algoritimo/Exercicios_TecVideo/01-operacoesAritmeticas.js
@@ -6,3 +6,23 @@
 // Divisão: 2 / 4 = x
 // Multiplicação: 2 * 4 = x
 // ---------------------------------------------------------------
+
+let num1 = 2;
+let num2 = 4;
+let x = 0;
+
+//soma
+x = num1 + num2;
+console.log(`soma: ${num1} + ${num2} = ${x}`);
+
+//subtração
+x = num1 - num2;
+console.log(`subitração: ${num1} - ${num2} = ${x}`);
+
+//divisão
+x = num1 / num2;
+console.log(`divisão: ${num1} / ${num2} = ${x}`);
+
+//multiplicação
+x = num1 * num2;
+console.log(`multiplicação: ${num1} * ${num2} = ${x}`);
